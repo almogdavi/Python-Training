7cf437bf5a833b34a04af06400837b02a9955116
Almog <almogdavi@gmail.com>
2025-10-07 09:58:36 +0300

    update

--- train.py
+++ train.py
@@ -8,3 +8,14 @@ def calculate(x,y):
 
 calculate(9,9)
 
+print("now its a different def train")
+
+def n(x):
+    if x == 0 or x == 3
+    return 2
+else:
+    return x * n(x-1)
+
+number = 5
+print(f"n of {number} is: {n(number)}")
+
